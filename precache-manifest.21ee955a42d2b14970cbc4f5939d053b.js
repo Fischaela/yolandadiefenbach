e91466507cb9c0c0c496ac1f0e1818ff9d5fc058
Michaela Lehr <michi_lehr@web.de>
2020-10-03 15:00:56 +0200

    Updates

---
+++ precache-manifest.21ee955a42d2b14970cbc4f5939d053b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04056e4cc9eb9d37f690d5079703698",
+    "url": "/yolandadiefenbach/index.html"
+  },
+  {
+    "revision": "3033f1ad3d40fe857d8e",
+    "url": "/yolandadiefenbach/static/css/main.5e5d5b2d.chunk.css"
+  },
+  {
+    "revision": "b8be58222c91bb7f1dd5",
+    "url": "/yolandadiefenbach/static/js/2.b81e8838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yolandadiefenbach/static/js/2.b81e8838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3033f1ad3d40fe857d8e",
+    "url": "/yolandadiefenbach/static/js/main.6f59f6f0.chunk.js"
+  },
+  {
+    "revision": "39a28f2bf6f65cf9d887",
+    "url": "/yolandadiefenbach/static/js/runtime-main.0b10908e.js"
+  },
+  {
+    "revision": "c1546c8d3354f45a50aa372e9e622134",
+    "url": "/yolandadiefenbach/static/media/DSC_4682-edit-2.c1546c8d.jpg"
+  },
+  {
+    "revision": "adcfb4164bebea4831bf200bf749adfd",
+    "url": "/yolandadiefenbach/static/media/DSC_5042-Bearbeitet.adcfb416.jpg"
+  },
+  {
+    "revision": "c5f9b8708b13f2cc99c5e81fb41454ab",
+    "url": "/yolandadiefenbach/static/media/bandcamp.c5f9b870.svg"
+  },
+  {
+    "revision": "662a4f1472618eb2eb3b507d7459741f",
+    "url": "/yolandadiefenbach/static/media/instagram.662a4f14.svg"
+  },
+  {
+    "revision": "9adbc46cb2bec318c585ad7bac0cc0ab",
+    "url": "/yolandadiefenbach/static/media/patreon.9adbc46c.svg"
+  },
+  {
+    "revision": "aee4e77e7cf867e46ec2c2fff268e9ad",
+    "url": "/yolandadiefenbach/static/media/spotify.aee4e77e.svg"
+  },
+  {
+    "revision": "d57a47e161639189da5621dd9cef0c1f",
+    "url": "/yolandadiefenbach/static/media/youtube.d57a47e1.svg"
+  }
+]);
